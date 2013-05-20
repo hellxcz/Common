00634edddde12c2b78b62a3cd251306cea688e30
zdenek.heller@seznam.cz <hellxcz@gmail.com>
2013-05-20 22:21:14 +0200

    Common.Cache introduced

---
+++ Common.Cache/FastSlowGet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InMemoryCache
+{
+    public class FastSlowGet
+    {
+        public static T Get<T>(Func<T> getFast, Func<T> getSlow)
+            where T : class
+        {
+            T item = getFast();
+
+            if (item != null)
+            {
+                return item;
+            }
+
+            {
+                item = getFast();
+
+                if (item != null)
+                {
+                    return item;
+                }
+
+                item = getSlow();
+
+                return item;
+            }
+        }
+    }
+}
